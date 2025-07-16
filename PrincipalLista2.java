9bdb0b694bd27e2635c74476e3fcdef5028994a7
Emanuele Fernanda Ferraz de Araújo <emanuelearaujo@ufpr.br>
2025-07-16 14:56:21 -0300

    Create PrincipalLista2.java

---
+++ PrincipalLista2.java
@@ -0,0 +1,18 @@
+public class PrincipalLista2 {
+    public static void main(String[] args) {
+        Pessoa[] pessoas = new Pessoa[2];
+        pessoas[0] = new Pessoa("Grace Hopper");
+        pessoas[0].ajustarData(9, 12, 1906);
+
+        pessoas[1] = new Pessoa("Ada Lovelace");
+        pessoas[1].ajustarData(10, 12, 1815);
+
+        for (Pessoa p : pessoas) {
+            p.calcularIdade(16, 7, 2025);
+            System.out.println("Nome: " + p.retornarNome());
+            System.out.print("Data de nascimento: ");
+            p.imprimirDataNascimento();
+            System.out.println("Idade: " + p.retornarIdade() + " anos\n");
+        }
+    }
+} 
